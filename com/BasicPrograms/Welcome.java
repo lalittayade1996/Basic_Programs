eef51a61e536f6adb13a7601290ec125656a156f
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-20 21:12:16 +0530

    Added : Welcome To Basic Programs

---
+++ com/BasicPrograms/Welcome.java
@@ -0,0 +1,29 @@
+package com.BasicPrograms;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO BASIC PROGRAMS OF JAVA");
+
+		System.out.println("             ");
+
+		System.out.println(" * Assignment Day 5 ");
+		System.out.println(" 1] Flip Coin Program");
+		System.out.println(" 2] Leap Number Program");
+		System.out.println(" 3] Power Of Two Program");
+		System.out.println(" 4] Harmonic Number Program");
+		System.out.println(" 5] Factors Program");
+		System.out.println(" 6] Swap Two Numbers Program");
+		System.out.println(" 7] Even Or Odd Number Program");
+		System.out.println(" 8] Largest Among Three Numbers Program");
+		System.out.println(" 9] Vowel Or Consonant Program");
+
+		System.out.println("             ");
+
+		System.out.println(" * Assignment Day 6 ");
+		System.out.println(" 1] Fibonacci Series Program");
+		System.out.println(" 2] Perfect Number Program");
+		System.out.println(" 3] Prime Number Program");
+		System.out.println(" 4] Reverse A Number Program");
+
+	}
+}
