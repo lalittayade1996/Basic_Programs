e1fd1ce7288cca0f8ee5fd06f5ff4413bbe31e42
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-20 21:29:52 +0530

    Added : FibonacciSeries Program

---
+++ com/BasicPrograms/FibonacciSeries.java
@@ -0,0 +1,30 @@
+package com.BasicPrograms;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		// scanner class declaration
+		Scanner sc = new Scanner(System.in);
+		// input from user
+		System.out.print("Enter the limit : ");
+		int lim = sc.nextInt();
+		if (lim > 0) {
+			int y = 0, z = 1, s;
+			// display starting two numbers of series
+			System.out.print("Fibonacci Series : " + y + "  " + z + "  ");
+			// perform iterations till the limit entered by the user
+			while (z <= lim) {
+				s = y + z;
+				y = z;
+				z = s;
+				// condition for forcing z that it should not be printed when its value is
+				// greater than limit
+				if (z <= lim)
+					System.out.print(z + "  ");
+			}
+		} else
+			System.out.print("Wrong Input");
+		// closing scanner class(not compulsory, but good practice)
+		sc.close();
+	}
+}
